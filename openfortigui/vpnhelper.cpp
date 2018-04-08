265d968e461673f0f437ef29b813fdfbe7fd3162
Rene Hadler <rene@hadler.me>
2018-04-08 15:14:18 +0200

    Added OpenSSL Aes128

--- openfortigui/vpnhelper.cpp
+++ openfortigui/vpnhelper.cpp
@@ -5,6 +5,10 @@
 #include "config.h"
 #include <qt5keychain/keychain.h>
 
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
 vpnHelper::vpnHelper()
 {
 
@@ -132,5 +136,113 @@ vpnHelperResult vpnHelper::systemPasswordStoreDelete(const QString &key)
 
     result.status = true;
     return result;
+}
+
+int vpnHelper::aes128_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int ciphertext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        ssl_handleErrors();
+
+    /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 128 bit AES (i.e. a 128 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
+        ssl_handleErrors();
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        ssl_handleErrors();
+    ciphertext_len = len;
+
+    /* Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        ssl_handleErrors();
+    ciphertext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int vpnHelper::aes128_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        ssl_handleErrors();
+
+    /* Initialise the decryption operation. IMPORTANT - ensure you use a key
+    * and IV size appropriate for your cipher
+    * In this example we are using 128 bit AES (i.e. a 128 bit key). The
+    * IV size for *most* modes is the same as the block size. For AES this
+    * is 128 bits */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
+        ssl_handleErrors();
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+    * EVP_DecryptUpdate can be called multiple times if necessary
+    */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        ssl_handleErrors();
+    plaintext_len = len;
+
+    /* Finalise the decryption. Further plaintext bytes may be written at
+    * this stage.
+    */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        ssl_handleErrors();
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
+
+QString vpnHelper::Qaes128_encrypt(const QString &plain, const QString &key, const QString &iv)
+{
+    QByteArray tmp;
+    tmp.resize(plain.length() * 10);
+    int ciphertext_len;
+
+    ciphertext_len = vpnHelper::aes128_encrypt((unsigned char *) plain.toStdString().c_str(), plain.toStdString().length(), (unsigned char *) key.toStdString().c_str(), (unsigned char *) iv.toStdString().c_str(), (unsigned char *) tmp.data());
+    tmp.resize(ciphertext_len);
+    return QString::fromUtf8(tmp.toBase64());
+}
+
+QString vpnHelper::Qaes128_decrypt(const QString &cipher, const QString &key, const QString &iv)
+{
+    QByteArray tmp, ci;
+    tmp.resize(cipher.length() * 10);
+    int decryptedtext_len;
+    ci = QByteArray::fromBase64(cipher.toUtf8());
+
+    decryptedtext_len = vpnHelper::aes128_decrypt((unsigned char *) ci.data(), ci.length(), (unsigned char *) key.toStdString().c_str(), (unsigned char *) iv.toStdString().c_str(), (unsigned char *) tmp.data());
+    tmp.resize(decryptedtext_len);
+    return QString::fromUtf8(tmp);
+}
+
+void vpnHelper::ssl_handleErrors()
+{
+    ERR_print_errors_fp(stderr);
 }
 
--- openfortigui/vpnhelper.h
+++ openfortigui/vpnhelper.h
@@ -21,6 +21,13 @@ public:
     static vpnHelperResult systemPasswordStoreWrite(const QString &key, const QString &data);
     static vpnHelperResult systemPasswordStoreRead(const QString &key);
     static vpnHelperResult systemPasswordStoreDelete(const QString &key);
+
+    static int aes128_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext);
+    static int aes128_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,unsigned char *iv, unsigned char *plaintext);
+    static QString Qaes128_encrypt(const QString &plain, const QString &key, const QString &iv);
+    static QString Qaes128_decrypt(const QString &cipher, const QString &key, const QString &iv);
+
+    static void ssl_handleErrors(void);
 };
 
 #endif // VPNHELPER_H
