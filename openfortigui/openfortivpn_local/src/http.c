b03eb57ff3342230c02ea5e2673177298df42bab
Rene Hadler <rene@hadler.me>
2018-06-01 07:42:03 +0200

    Added GPLv3 license

--- openfortigui/openfortivpn_local/src/http.c
+++ openfortigui/openfortivpn_local/src/http.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2015 Adrien Vergé
+ *  Copyright (C) 2018 Adrien Vergé, Rene Hadler
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
